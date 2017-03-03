47adde450c9805daa30007f793d5e069f494dc62
curso04 <curso04java@gmail.com>
2017-03-03 14:55:19 +0100

    Añadiendo la clase "Autor" para llevar un mantenimiento de la misma

---
+++ src/main/java/es/cic/curso/curso04/ejercicio028/backend/dominio/Autor.java
@@ -0,0 +1,108 @@
+package es.cic.curso.curso04.ejercicio028.backend.dominio;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import es.cic.curso.curso04.ejercicio028.backend.repository.Identificable;
+
+@Entity
+@Table(name="AUTOR")
+public class Autor implements Identificable<Long> {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
+	@Column(name="NOMBRE")
+	private String nombre;
+	
+	@Column(name="APELLIDOS")
+	private String apellidos;
+	
+	@Column(name="FECHA_NACIMIENTO")
+	private int fechaNacimiento;
+	
+	public Autor() {
+		super();
+	}
+
+	public Autor(String nombre, String apellidos, int fechaNacimiento) {
+		super();
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	@Override
+	public Long getId() {
+		return id;
+	}
+
+	@Override
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public int getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(int fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Autor other = (Autor) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Autor [id=" + id + ", nombre=" + nombre + ", apellidos=" + apellidos + ", fechaNacimiento="
+				+ fechaNacimiento + "]";
+	}
+
+	
+}
